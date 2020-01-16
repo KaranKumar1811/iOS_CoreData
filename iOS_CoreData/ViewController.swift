98220a2f9f97117bc5a4117f9827c79ad47aac3b
Karan <kumarkay.ca@gmail.com>
2020-01-16 15:42:10 -0500

    init

--- iOS_CoreData/ViewController.swift
+++ iOS_CoreData/ViewController.swift
@@ -7,13 +7,58 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        // First we create an Instance of app delegate
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        /*
+         second we need the context
+         this context is the manager like location manager ,audio manage etc
+         */
+        let context = appDelegate.persistentContainer.viewContext
+        
+        //3rd step write into context
+
+        let newUser = NSEntityDescription.insertNewObject(forEntityName: "User", into: context)
+
+        newUser.setValue("sandy", forKey: "name")
+        newUser.setValue(787353568, forKey: "phone")
+        newUser.setValue("sandy.ca@gmail.com", forKey: "email")
+        newUser.setValue(22, forKey: "age")
+        
+       //  4th step - save context
+        do{
+           try context.save()
+            print(newUser ," is saved")
+        }catch{
+            print(error)
+        }
+        
+        //fetch data and load it
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "User")
+        // Find Our Data
+        do{
+            let result = try context.fetch(request)
+            if result.count > 0{
+                for results in result as! [NSManagedObject]{
+                    if let name = results.value(forKey: "name"){
+                    print(name)
+                    }
+                    
+                }
+            }
+        }
+        catch{
+            print(error)
+        }
     }
+    
 
 
 }
